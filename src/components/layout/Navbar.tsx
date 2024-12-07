a240d768ed7f4ede9259bf62ab97ec3a97da03ca
Mhd Qais Alsaleh <qaisalsaleh251@gmail.com>
2024-12-07 13:03:06 +0200

    Update Navbar component to reflect name change from 'Portfolio' to 'Qais Alsaleh'

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <Link to="/" className="text-xl font-bold text-gray-800">
-              Portfolio
+              Qais Alsaleh
             </Link>
           </div>
           {/* Mobile Navigation */}
