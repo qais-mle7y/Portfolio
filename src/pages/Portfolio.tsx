991ae46635d02c15c365aca80a8ba73ba832cf9b
Mhd Qais Alsaleh <qaisalsaleh251@gmail.com>
2024-12-17 14:21:41 +0200

    Update Portfolio.tsx

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProjectCard from '../components/portfolio/ProjectCard';
 import campusSafetyImage from '../assets/campusSafety.png';
+import commonGrounds from '../assests/commonGrounds.png';
 
 const Portfolio = () => {
   const projects = [
@@ -15,7 +16,7 @@ const Portfolio = () => {
     {
       title: 'Common Grounds App',
       description: 'This project aims to equip the body corporates of sectional titles with a platform that seeks to make the execution of their responsibilities easy and seamless.',
-      image: 'https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      image: commonGrounds,
       technologies: ['JavaScript', 'HTML', 'CSS', 'Node.js', 'MongoDB', 'Express'],
       githubUrl: 'https://github.com/danishx99/common-grounds-app',
       demoUrl: '',
